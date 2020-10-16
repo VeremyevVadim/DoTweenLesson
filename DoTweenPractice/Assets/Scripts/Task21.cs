811c3c2c65390f338f70c71c2eddf834241aa02a
VeremyevVadim <qwertyvadimv@mail.ru>
2020-10-16 12:52:35 +0300

    Scene 'Task_2' added

---
+++ DoTweenPractice/Assets/Scripts/Task21.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Task21 : MonoBehaviour
+{
+    [SerializeField] private Transform[] circles;
+
+    public void OnClickEvent()
+    {
+        Sequence sequence = DOTween.Sequence();
+
+        var distance = 15f;
+        var duration = 0.5f;
+        var position = circles[0].position;
+        sequence.Append(circles[1].DOMove(position + Vector3.right * distance, duration));
+        sequence.Join(circles[2].DOMove(position + Vector3.right * distance * 2, duration));
+        sequence.Append(circles[1].DOMove(position - Vector3.right * distance, duration));
+        sequence.Join(circles[2].DOMove(position - Vector3.right * distance * 2, duration));
+        sequence.Append(circles[1].DOMove(position, duration));
+        sequence.Join(circles[2].DOMove(position, duration));
+        sequence.OnComplete(OnClickEvent);
+    }
+}
---
+++ DoTweenPractice/Assets/Scripts/Task22.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Task22 : MonoBehaviour
+{
+    [SerializeField] private Transform[] rects;
+    
+    public void OnClickEvent()
+    {
+        Sequence sequence = DOTween.Sequence();
+
+        var smallScale = new Vector3(1f, 0.6f, 1f);
+        var duration = 0.25f;
+
+        foreach (var rect in rects)
+        {
+            sequence.Append(rect.DOScale(smallScale, duration));
+        }
+        
+        foreach (var rect in rects)
+        {
+            sequence.Append(rect.DOScale(Vector3.one, duration));
+        }
+        
+        sequence.OnComplete(OnClickEvent);
+    }
+}
